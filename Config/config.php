427c84c92ed1ba449fa6523e4ab620d0f6e7ba33
Robert Golebiewski <rag.web@outlook.com>
2026-02-09 12:46:37 +0100

    Refactor service configuration for Unosend SMS integration

--- Config/config.php
+++ Config/config.php
@@ -9,7 +9,25 @@ return [
     'services' => [
         'integrations' => [
             'mautic.integration.unosendsms' => [
-                'class' => \MauticPlugin\UnosendSmsBundle\Integration\UnosendSmsIntegration::class,
+                'class'     => \MauticPlugin\UnosendSmsBundle\Integration\UnosendSmsIntegration::class,
+                'arguments' => [
+                    'event_dispatcher',
+                    'mautic.helper.cache_storage',
+                    'doctrine.orm.entity_manager',
+                    'request_stack',
+                    'router',
+                    'translator',
+                    'monolog.logger.mautic',
+                    'mautic.helper.encryption',
+                    'mautic.lead.model.lead',
+                    'mautic.lead.model.company',
+                    'mautic.helper.paths',
+                    'mautic.core.model.notification',
+                    'mautic.lead.model.field',
+                    'mautic.plugin.model.integration_entity',
+                    'mautic.lead.model.dnc',
+                    'mautic.lead.field.fields_with_unique_identifier',
+                ],
             ],
         ],
         'others' => [
